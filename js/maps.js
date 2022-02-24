f848fe8ac58b30fe3ccf771b2cf176af956877cc
Stephen <glath.stephen@gmail.com>
2022-02-24 17:25:11 +0100

    maion

--- js/maps.js
+++ js/maps.js
@@ -6,7 +6,7 @@ function initMap()
     const uluru = { lat: 45.769, lng: 4.830 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
+      zoom: 17,
       center: uluru,
     });
     // The marker, positioned at Uluru
