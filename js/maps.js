4f15a9128d77bb02d167d967aac2d9d25847c5e0
Stephen <glath.stephen@gmail.com>
2022-02-24 16:34:45 +0100

    maj

---
+++ js/maps.js
@@ -0,0 +1,17 @@
+// Initialize and add the map
+function initMap()
+{
+    // The location of Uluru
+    // 45.769668654516764, 4.830151793709206
+    const uluru = { lat: 45.769, lng: 4.830 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+}
